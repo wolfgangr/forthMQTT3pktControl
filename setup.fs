fde44a14c90455f3b25282b2ecd7ae1993702c3c
Wolfgang Rosner <wrosner@tirnet.de>
2019-01-27 15:17:19 +0100

    include threaded timed lib

--- setup.fs
+++ setup.fs
@@ -1,6 +1,15 @@
 include rnw/h
 include rnw/cornerstone.fs
 cornerstone <<embello-hal>>
+
+\ https://github.com/jeelabs/embello/blob/master/docs/flib/timed.md#callback-timer-library
+include ../flib/mecrisp/multi.fs
+cornerstone <<multitasking-lib>>
+
+include ../flib/any/timed.fs
+cornerstone <<callback-timer-lib>>
+
 include rnw/blink.fs
 cornerstone <<blink-test>>
+
 
