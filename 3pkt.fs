38476559453d53d836a69ec5e82a3bebf5ebb435
Wolfgang Rosner <wrosner@tirnet.de>
2019-01-27 10:53:14 +0100

    3-punkt funzt

--- 3pkt.fs
+++ 3pkt.fs
@@ -7,11 +7,14 @@ include rel.fs
 2500 variable limit-v-off
 1000 variable limit-p-on
 1500 variable limit-p-off
+100 variable pv-loop-delay
 
-\===================
-: pumpe? ( druck --) dup limit-p-on @ < IF pumpen THEN limit-p-off @  > IF pv-stop THEN ;
-: ventil? ( druck --) dup limit-v-on @ > IF ablassen THEN limit-v-off @  < IF pv-stop THEN ;
-: pv-check druck@ dup pumpe? ventil? ;
-: pv-loop BEGIN pv-check key? UNTIL ;
-: pv-test BEGIN druck@ dup . dup pumpe? ventil? key? 1000 ms UNTIL ;
+\ ===================
+: pumpe? ( druck --) limit-p-on @ < IF pumpen THEN ;     \    limit-p-off @  > IF pv-stop THEN ;
+: ventil? ( druck --) limit-v-on @ > IF ablassen THEN ;  \    limit-v-off @  < IF pv-stop THEN ;
+: pvOK? ( druck --) dup limit-p-off @  >  swap limit-v-off @  <  and IF pv-stop THEN ;
+
+: pv-check druck@ dup dup pumpe? ventil? pvOK? ;
+: pv-loop BEGIN pv-check key? pv-loop-delay @ ms  UNTIL ;
+: pv-test BEGIN druck@ dup . dup dup pumpe? ventil? pvOK? key? 1000 ms UNTIL ;
 
