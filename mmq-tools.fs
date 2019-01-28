fa39d10ee65b8af37a18f41165aee5996a597c87
Wolfgang Rosner <wrosner@tirnet.de>
2019-01-28 01:34:35 +0100

    literal byte strings

--- mmq-tools.fs
+++ mmq-tools.fs
@@ -9,4 +9,10 @@
 \ with leading length byte
 : string," $22 parse dup IF string, ELSE 2drop THEN ;
 
+\ put literal bytes to Dictionary, depnding on current base
+\ bytes," 00 11 AA BB 00 FF "
+\ token           ( -- c-addr len ) Cuts one token out of input buffer
+: bytes," BEGIN token over c@ $22 <>  WHILE  number 1 <> IF  quit THEN c,  REPEAT 2drop  ; 
+
+\ don't forget to call calign afterwards
 
