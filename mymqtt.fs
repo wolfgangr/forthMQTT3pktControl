4b67a6753f7145dcf90fad18fa44162738e2fe65
Wolfgang Rosner <wrosner@tirnet.de>
2019-01-31 18:53:12 +0100

    test with both variants

--- mymqtt.fs
+++ mymqtt.fs
@@ -2,19 +2,9 @@ include mmq-const.fs
 include mmq-tools.fs
 
 \ should go to lib
-: my.stringbuf-type ( adr -- ) stringbuf-string type ; 
-
-$40 stringbuffer constant mqtt-message
-\ mqtt-message MQTT-init 
-\ mqtt-message mymq.prefix mymq.valve MQTT-topic 
-\ mqtt-message MQTT-append-off    .... -on
-\ mqtt-message MQTT-append-q.a.r 
-\ mqtt-message stringbuf-dump 
+\ : my.stringbuf-type ( adr -- ) stringbuf-string type ; 
 
 
-$80 stringbuffer constant slip-message  
-\ mqtt-message mymq.valve.off 
-\ slip-message mqtt-message  stringbuf-string SLIP-assemble
 
 
 : mymq.prefix s" heating/pressure" ;
@@ -56,5 +46,24 @@ $80 stringbuffer constant slip-message
   
   
   
+\ test rund
   
-  
+$40 stringbuffer constant mqtt-message
+$80 stringbuffer constant slip1-message
+$80 stringbuffer constant slip2-message 
+
+
+
+\ mqtt-message MQTT-init 
+\ mqtt-message mymq.prefix mymq.valve MQTT-topic 
+\ mqtt-message MQTT-append-off    .... -on
+\ mqtt-message MQTT-append-q.a.r 
+\ mqtt-message stringbuf-dump 
+
+
+\ $80 stringbuffer constant slip-message  
+mqtt-message mymq.valve.off 
+slip1-message mqtt-message  stringbuf-string SLIP-assemble
+
+\ repeat old stuff:
+slip2-message MQTT-washer.topic memstr-counted MQTT-msg.off memstr-counted MQTT-assemble
