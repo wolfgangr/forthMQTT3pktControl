8aff93a1452c289d54c6a3c6108cd077842701a2
Wolfgang Rosner <wrosner@tirnet.de>
2019-02-03 14:38:12 +0100

    always return to compiler

--- slip-key.fs
+++ slip-key.fs
@@ -11,7 +11,7 @@ $100 stringbuffer constant test-buf
 
 \ try to read another character
 \ send 0x0D , wait 10 ms, maybe wait another configurable time and then call an error
-: sys-key-timed
+: sys-key-timed-###
     sys-key? not IF  
        \ $11 sys-emit 
        100 ms
@@ -21,7 +21,22 @@ $100 stringbuffer constant test-buf
     THEN THEN THEN
   sys-key
 ; 
-  
+
+
+: slip-key-timeout
+  sys-key? IF exit THEN
+  100 ms
+  sys-key? IF exit THEN
+  SLIP-timeout @ ms 
+  sys-key? IF exit THEN
+  SLIP-timeout-error
+; 
+
+: sys-key-timed
+  slip-key-timeout
+  sys-key
+;
+
 : SLIP-key-unescape ( char -- char )
   dup SLIP_ESC = sys-key? and IF 
       sys-key-timed nip 
@@ -66,11 +81,14 @@ $100 stringbuffer constant test-buf
       
     THEN
              ( char flag - )
-    dup IF
-    ELSE
-      nip sys-key-timed swap 
-    THEN
+    \ dup IF
+    \ ELSE
+    \   nip sys-key-timed swap 
+    \ THEN
     over   debug
+        \ hand over 0 as dummy to compiler and return in any case
+    not if drop 0 then
+    true
   UNTIL
   
 ; 
@@ -91,3 +109,24 @@ $100 stringbuffer constant test-buf
 \ test-buf stringbuf-clear  ok.
 \ slip-message stringbuf-clear  ok.
 \ 5000 slip-timeout ! 
+
+: run-test
+  ." =========================== running test ================="
+  test-buf stringbuf-dump
+  test-buf stringbuf-clear
+  slip-message stringbuf-dump
+  slip-message stringbuf-clear 
+ 
+  ." ----------------------- start test -------------------------"
+
+  ['] SLIP-key hook-key !
+  ESPL-sync memstr-counted slip-send 
+  ['] sys-key hook-key !
+
+  ." ---------------------------finished test --------------------"
+
+  test-buf stringbuf-dump
+  slip-message stringbuf-dump
+  
+  ." =============================test finished =================="
+;
